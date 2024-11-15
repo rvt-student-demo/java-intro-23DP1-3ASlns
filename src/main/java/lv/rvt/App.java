17d93899281eba0f843c877f85806d227dae00e7
23DP1-3ASiln <165785440+23DP1-3ASlns@users.noreply.github.com>
2024-11-15 15:56:51 +0000

    PaymentCard update

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,26 +4,15 @@ import java.util.ArrayList;
 import java.util.Random;
 public class App
 {
-           public static void main(String[] args) {
-            PaymentCard card = new PaymentCard(50);
-            System.out.println(card);
-
-            card.eatAffordably();
-            System.out.println(card);
-
-            card.eatHeartily();
-            card.eatAffordably();
-            System.out.println(card);
-
-            
-        card.addMoney(15);
+    public static void main(String[] args) {
+        PaymentCard card = new PaymentCard(50);
         System.out.println(card);
 
-        card.addMoney(10);
+        card.eatAffordably();
         System.out.println(card);
 
-        card.addMoney(200);
+        card.eatHeartily();
+        card.eatAffordably();
         System.out.println(card);
-        }
-    
+    }
 }
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -3,43 +3,28 @@ package lv.rvt;
 public class PaymentCard {
     private double balance;
 
-    // Constructor that initializes the balance with the opening balance
     public PaymentCard(double openingBalance) {
         this.balance = openingBalance;
     }
 
-    // Method that returns the card's balance in the specified format
-    @Override
-    public String toString() {
-        return "The card has a balance of " + this.balance + " euros";
+    public void eatAffordably() {
+        double costAffordable = 2.50;
+        if (this.balance >= costAffordable)[
+            this.balance -= costAffordable;
+        ]
     }
-}
+    
+    public void eatHeartily() {
+        double costHeart = 5;
+        if (this.balance >= costHeart){
+            this.balance -= costHeart;
 
-public void eatAffordably() {
-    // write code here
-    double eatAffordably = 2.60;
-    if (this.balance >= eatAffordably){
-        this.balance -= eatAffordably;
-    }
-}
-
-public void eatHeartily() {
-    // write code here
-    double eatHeartily = 4.60;
-    if (this.balance >= eatHeartily){
-        this.balance -= eatHeartily;
-    }
-}
-
-public double getBalance(){
-    return this.balance;
-}
-
-public void addMoney(double amount) {
-    if (amount > 0) {
-        this.balance += amount;
-        if (this.balance > 150) {
-            this.balance = 150;
         }
     }
+
+    public String toString() {
+        return "Card balance: " + this.balance + "€";
+    }
+
+
 }
