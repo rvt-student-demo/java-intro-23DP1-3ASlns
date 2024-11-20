388e3b2416237640e8d1589303ea7f74c263a7c1
23DP1-3ASiln <165785440+23DP1-3ASlns@users.noreply.github.com>
2024-11-20 12:46:13 +0000

    book

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,18 +1,40 @@
 package lv.rvt;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Random;
+
 public class App
 {
-    public static void main(String[] args) {
-        PaymentCard card = new PaymentCard(50);
-        System.out.println(card);
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+ArrayList<Book> books = new ArrayList<>();
 
-        card.eatAffordably();
-        System.out.println(card);
+while (true) {
+System.out.print("Title: ");
+String title = scanner.nextLine();
+if (title.isEmpty()) {
+break;
+}
 
-        card.eatHeartily();
-        card.eatAffordably();
-        System.out.println(card);
-    }
+System.out.print("Pages: ");
+int pages = Integer.parseInt(scanner.nextLine());
+
+System.out.print("Publication year: ");
+int publicationYear = Integer.parseInt(scanner.nextLine());
+
+books.add(new Book(title, pages, publicationYear));
+}
+
+System.out.print("What information will be printed? ");
+String choice = scanner.nextLine();
+
+if (choice.equals("everything")) {
+for (Book book : books) {
+System.out.println(book);
+}
+} else if (choice.equals("name")) {
+for (Book book : books) {
+System.out.println(book.getTitle());
+}
+}
+}
 }
